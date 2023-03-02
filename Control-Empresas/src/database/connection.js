84f205f094099eb9335b14eb9e7e026a5a30e6a6
Noslen <nlopez-2019450@kinal.edu.gt>
2023-03-01 23:47:14 -0600

    Se Creó la conexión a la base de datos

--- Control-Empresas/src/database/connection.js
+++ Control-Empresas/src/database/connection.js
@@ -1,14 +1,15 @@
-const mongoose = require ("mongoose")
-mongoose.set('strictQuery', true);
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true );
 
 const connection = async() =>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/VentaOnline')
+        await mongoose.connect('mongodb://127.0.0.1:27017/ControlEmpresas')
         console.log('Conectado a la base de datos!! OwO')
-    }catch(error){
-        console.log(error)
-        throw new Error ('Error al conectar a la base de datos')
+    }catch(err){
+        console.log(err)
+        throw new Error('Error al iniciar la db');
     }
 }
 
-module.exports = {connection}
+
+module.exports = { connection }
